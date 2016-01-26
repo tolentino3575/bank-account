5f2dc11be9f8bc80841e18710ae0448caaf9cec3
Alex Larson and Erik Tolentino <alex.m.larson@gmail.com>
2016-01-26 14:19:52 -0800

    Add jquery and javascript functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,46 @@
-function Account(userName, deposit) {
+function Account(userName, initialDeposit) {
   this.userName = userName;
-  this.deposit = deposit;
+  this.initialDeposit = initialDeposit;
 }
+
+Account.prototype.deposits = function(deposit) {
+  return this.initialDeposit + deposit;
+}
+
+Account.prototype.withdrawls = function(withdrawl) {
+  return this.initialDeposit - withdrawl;
+}
+
+
+
+$(function () {
+$("#signup").submit(function (event) {
+  var name = $("#yourName").val();
+  var balance = parseInt($("#initialDeposit").val());
+  var newAccount = new Account(name, balance);
+  console.log(newAccount);
+  $(".balance").text(newAccount.initialDeposit);
+  $(".accountName").text(newAccount.userName);
+  event.preventDefault();
+
+
+  $("#bankDeposit").click(function(){
+    var deposit = parseInt($("#deposit").val());
+    var newDeposit = (newAccount.initialDeposit + deposit);
+    newAccount.initialDeposit = newDeposit;
+    $(".balance").text(newDeposit);
+  })
+
+  $("#bankWithdraw").click(function(){
+    var withdraw = parseInt($("#withdraw").val());
+    var newDeposit = (newAccount.initialDeposit - withdraw);
+    newAccount.initialDeposit = newDeposit;
+    $(".balance").text(newDeposit);
+  })
+
+
+})
+
+
+
+})
--- spec/specs.js
+++ spec/specs.js
@@ -2,6 +2,17 @@ describe('Account', function() {
   it("will display name and initial amount", function () {
     var testAccount = new Account("Erik", 10);
     expect(testAccount.userName).to.equal("Erik");
-    expect(testAccount.deposit).to.equal(10);
+    expect(testAccount.initialDeposit).to.equal(10);
   });
+
+ it("will add to account balance", function (){
+    var testAccount = new Account("Erik", 10);
+    expect(testAccount.deposits(5)).to.equal(15);
+ });
+
+ it("will subract from account balance", function () {
+   var testAccount = new Account("Erik", 10);
+   expect(testAccount.withdrawls(5)).to.equal(5);
+ });
+
 });
